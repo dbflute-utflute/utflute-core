6b3b8fd52ebd192ff09829c4430a2646918be693
jflute <dbflute@gmail.com>
2014-11-24 23:39:44 +0900

    Release DBFlute-0.5.0-RC1

--- utflute-base/maven-deploy.sh
+++ utflute-base/maven-deploy.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Base
-mvn -e clean deploy install
+mvn -e clean deploy -Dgpg.keyname=$1
 
 # Core
 cd ../utflute-core
-. maven-deploy.sh
+. maven-deploy.sh $1
 
 # Mocklet
 cd ../../utflute-mocklet/utflute-mocklet
-. maven-deploy.sh
+. maven-deploy.sh $1
 
 # Seasar
 cd ../../utflute-seasar/utflute-seasar
-. maven-deploy.sh
+. maven-deploy.sh $1
 
 # Spring
 cd ../../utflute-spring/utflute-spring
-. maven-deploy.sh
+. maven-deploy.sh $1
 
 cd ../utflute-spring-web
-. maven-deploy.sh
+. maven-deploy.sh $1
 
 # Guice
 cd ../../utflute-guice/utflute-guice
-. maven-deploy.sh
+. maven-deploy.sh $1
 
 cd ../utflute-guice-web
-. maven-deploy.sh
+. maven-deploy.sh $1
