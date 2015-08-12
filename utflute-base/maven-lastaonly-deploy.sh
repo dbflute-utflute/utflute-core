58f13c58b98e9727708dc026ab7596625456a589
jflute <dbflute@gmail.com>
2015-08-12 22:42:25 +0900

    show UTFlute-0.5.0-sp8-RC2

---
+++ utflute-base/maven-lastaonly-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Base
+mvn -e clean deploy -Dgpg.keyname=$1
+
+# Core
+cd ../utflute-core
+. maven-deploy.sh $1
+
+# Mocklet
+cd ../../utflute-mocklet/utflute-mocklet
+. maven-deploy.sh $1
+
+# LastaFlute
+cd ../../utflute-lasta/utflute-lasta-di
+. maven-deploy.sh $1
+
+cd ../utflute-lastaflute
+. maven-deploy.sh $1
